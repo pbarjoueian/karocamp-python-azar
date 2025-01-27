fce4ac48628402758c60b7b83424c20a02baf9be
Peyman Barjoueian <p.barjoueian@gmail.com>
2025-01-27 18:38:14 +0330

    Uncomment some codes

--- python_db.py
+++ python_db.py
@@ -78,39 +78,39 @@
 #         print("PostgreSQL connection is closed")
 
 
-# import psycopg2
+import psycopg2
 
-# try:
-#     connection = psycopg2.connect(
-#         user="postgres",
-#         password="postgres",
-#         host="127.0.0.1",
-#         port="5432",
-#         database="karocamp_db",
-#     )
-#     cursor = connection.cursor()
-#     postgreSQL_select_Query = "select * from publisher"
+try:
+    connection = psycopg2.connect(
+        user="postgres",
+        password="postgres",
+        host="127.0.0.1",
+        port="5432",
+        database="karocamp_db",
+    )
+    cursor = connection.cursor()
+    postgreSQL_select_Query = "select * from publisher"
 
-#     cursor.execute(postgreSQL_select_Query)
-#     print("Selecting rows from publisher table using cursor.fetchall")
-#     publisher_records = cursor.fetchall()
+    cursor.execute(postgreSQL_select_Query)
+    print("Selecting rows from publisher table using cursor.fetchall")
+    publisher_records = cursor.fetchall()
 
-#     print("Print each row and it's columns values")
-#     for row in publisher_records:
-#         print(
-#             "publisher_Id = ",
-#             row[0],
-#         )
-#         print("publisher_name = ", row[1])
-#         print("publisher_estd  = ", row[2])
-#         print("publisher_location  = ", row[3])
-#         print("publisher_type  = ", row[4], "\n")
-# except (Exception, psycopg2.Error) as error:
-#     print("Error while fetching data from PostgreSQL", error)
+    print("Print each row and it's columns values")
+    for row in publisher_records:
+        print(
+            "publisher_Id = ",
+            row[0],
+        )
+        print("publisher_name = ", row[1])
+        print("publisher_estd  = ", row[2])
+        print("publisher_location  = ", row[3])
+        print("publisher_type  = ", row[4], "\n")
+except (Exception, psycopg2.Error) as error:
+    print("Error while fetching data from PostgreSQL", error)
 
-# finally:
-#     # closing database connection.
-#     if connection:
-#         cursor.close()
-#         connection.close()
-#         print("PostgreSQL connection is closed")
+finally:
+    # closing database connection.
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
